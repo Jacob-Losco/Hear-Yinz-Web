cf35c6a0afc5c478f9c571ff0e881fa364837145
Sam M <122312011+Samuel-Merlin@users.noreply.github.com>
2023-02-02 13:16:41 -0500

    LoginPush

--- web-dashboard/src/Login.js
+++ web-dashboard/src/Login.js
@@ -1,28 +1,64 @@
 
-import React from "react";
-import "@fontsource/dm-sans";
+
+import { onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import React, { useState, useEffect } from 'react';
+import { oAuthentication } from './firebase-config';
+import '@fontsource/dm-sans';
 import './font.css';
 import './Login.css';
 
 function Login() {
-      return <div className="FirebaseLogin" >
+  const [sLoginEmail, fnSetLoginEmail] = useState("");
+  const [sLoginPassword, fnSetLoginPassword] = useState("");
+
+  /*F+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  Function: fnLogin
+  Summary: authenticates user from firebase authentication based on current set email and password, or rejects them and prints an error message
+  Args: None
+  Returns: None
+-------------------------------------------------------------------F*/
+  const fnLogin = async () => {
+      try {
+          const oUser = await signInWithEmailAndPassword(
+              oAuthentication, 
+              sLoginEmail, 
+              sLoginPassword
+          );
+      } 
+      catch (error) {
+          console.log(error.message);
+          const Message = document.getElementById('Message');
+          Message.innerHTML = "Invalid email, please enter an associated email or contact administration!";
+      }
+  }
+  /*F+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  Function: fnLogout
+  Summary: signs out a currently signed in user.
+  Args: None
+  Returns: None
+-------------------------------------------------------------------F*/
+      return <div >
     <div className='container'>
       <h2> Hear Yinz! </h2>
       <h1> Admin Dashboard </h1>
       <div>
-        <div><b>Email</b></div>
-        <input className = "inpt" placeholder="Email"/>
+        <div className='bld'>Email</div>
+        <input className = "inpt" placeholder='Email' onChange={(event) => {
+        fnSetLoginEmail(event.target.value);
+      }} />
       </div>
       <br></br>
       <div>
-        <div><b>Password</b></div>
-        <input className = "inpt" placeholder="Password"/>
+        <div className='bld' >Password</div>
+        <input className = "inpt" placeholder='Password' onChange={(event) => {
+        fnSetLoginPassword(event.target.value);
+      }} />
       </div>
       <br></br>
       <br></br>
-      <button className = 'btn'>Log in</button>
+      <button className = 'btn' onClick={fnLogin} >Log in</button>
+      <p id ='Message'></p>
       </div>
-  </div>
+  </div> 
 }
-
 export default Login;
