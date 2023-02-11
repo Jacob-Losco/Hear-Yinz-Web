b134b404e85a5252f7255d1867ffd47f0e4cb279
Sam M <samuel.merlin@stvincent.edu>
2023-02-11 16:49:15 -0500

    for images

--- web-dashboard/src/Pages/Organizations.js
+++ web-dashboard/src/Pages/Organizations.js
@@ -9,17 +9,23 @@ Exported Functions: Organizations
 
 Contributors:
 	Philip Pavlick - 02/2/23 - SP-263
+    Sam Merlin - 02/10/23
 
 ===================================================================+*/
 
-import React from 'react';
+import React from "react";
+import Grid from '@mui/material/Grid';
+import logo from './Recources /HearYinzLogo.png'
 
+      
 export default function Organizations() {
     return(
-        // used to query the organization page
-        <div className="OrganizationPage">  
-            <h>Welcome Organizations</h>
-            <p>Sample text from Organizations page.</p>
-        </div>
-    );
-}
+        <Grid container spacing={{ xs: 5, md: 3 }} columnSpacing={ 4 } >
+        {Array.from(Array(9)).map((_, index) => (
+          <Grid  alignItems='center' item xs={6} sm={4} md={4} key={index}>
+            <img src={logo} width={234} height={234} />
+            <div>Click for the form!</div>
+          </Grid>
+        ))}
+      </Grid>
+  )};
