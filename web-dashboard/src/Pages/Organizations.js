d7dab4e5901e017b162ad323eadffb5c8c49d43f
Sam M <samuel.merlin@stvincent.edu>
2023-02-18 12:11:26 -0500

    BugFix

--- web-dashboard/src/Pages/Organizations.js
+++ web-dashboard/src/Pages/Organizations.js
@@ -9,12 +9,14 @@ Exported Functions: Organizations
 
 Contributors:
 	Philip Pavlick - 02/2/23 - SP-263
-  Sam Merlin - 02/10/23
+  Sam Merlin - 02/18/23 - SP-267
 ===================================================================+*/
 import React, { useState } from 'react';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 import './OrgList.css'      
+import {Link,} from 'react-router-dom'
 import { useEffect } from 'react';  
 import { fnGetOfficerOrganizations} from '../DBFunctions';
 import { onAuthStateChanged } from 'firebase/auth';
@@ -41,13 +43,17 @@ export default function Organizations() {
       <Grid container spacing={{ xs: 9, md: 5 }} columnSpacing = {4}>
         {iOrganizations.map(iOrganization => (
           <Grid  textAlign='center' item xs={5} sm={4} md={3} key={iOrganization.id}>
+            <Button component={Link} to ="OrgEventsPage">
             <div>
               <img className='img' src={iOrganization.img} width={150} height={150} />
             </div>
+            </Button>
             <div>{iOrganization.name}</div>
+
           </Grid>
         ))}
       </Grid>
     </Box>
+
   )
 };
