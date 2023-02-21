4284f9aabfe5527c2d6e92e20b0b56a2dc69cac6
Sam M <samuel.merlin@stvincent.edu>
2023-02-20 19:33:50 -0500

    Revert "Merge branch 'main' into Merlin_Org_SubNavBar"
    This reverts commit bfecc666adee266dc2c22ecd9c0543641d5d3b77, reversing
    changes made to 5120df3a4afdd950d552ec13813c5bf5791cb97d.

--- web-dashboard/src/Pages/Organizations.js
+++ web-dashboard/src/Pages/Organizations.js
@@ -9,14 +9,19 @@ Exported Functions: Organizations
 
 Contributors:
 	Philip Pavlick - 02/2/23 - SP-263
-  Sam Merlin - 02/18/23 - SP-267
+  Sam Merlin - 02/10/23
 ===================================================================+*/
 import React, { useState } from 'react';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import './OrgList.css'      
-import {Link} from 'react-router-dom';
+import {Link, Route, Routes} from 'react-router-dom';
+import OrgE from './OrgE';
+import OrgAnn from './OrgAnn';
+import OrgAU from './OrgAU';
+import OrgOff from './OrgOff';
+
 import { useEffect } from 'react';  
 import { fnGetOfficerOrganizations} from '../DBFunctions';
 import { onAuthStateChanged } from 'firebase/auth';
@@ -31,7 +36,6 @@ export default function Organizations() {
       let oOrgs = await fnGetOfficerOrganizations();
       setiOrganizations(oOrgs)
     }
-    
     onAuthStateChanged(oAuthentication, (oCurrentUser) => {          
       if(oCurrentUser != null) {
         fnUpdateOrganizations()
@@ -44,16 +48,23 @@ export default function Organizations() {
       <Grid container spacing={{ xs: 9, md: 5 }} columnSpacing = {4}>
         {iOrganizations.map(iOrganization => (
           <Grid  textAlign='center' item xs={5} sm={4} md={3} key={iOrganization.id}>
-            <Button component={Link} to ="OrgEventsPage">
+            <Button component={Link} to ="Events">
             <div>
               <img className='img' src={iOrganization.img} width={150} height={150} />
             </div>
             </Button>
             <div>{iOrganization.name}</div>
+              <div>
+                <Routes>
+                    <Route path='/Events' element={<OrgE/>} />
+                    <Route path='/AboutUs' element={<OrgAU/>} />
+                    <Route path='/Announcements' element={<OrgAnn/>} />
+                    <Route path='/Officers' element={<OrgOff/>}/>
+                </Routes>
+              </div>
           </Grid>
         ))}
       </Grid>
     </Box>
-
   )
 };
