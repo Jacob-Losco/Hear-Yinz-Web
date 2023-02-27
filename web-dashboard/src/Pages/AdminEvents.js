338c11278c636399db333022cb6b3d1f36dda057
Philip-Pavlick1 <122839348+Philip-Pavlick1@users.noreply.github.com>
2023-02-27 17:55:37 -0500

    Added Functionality to Admin Events
    Added moment package.

--- web-dashboard/src/Pages/AdminEvents.js
+++ web-dashboard/src/Pages/AdminEvents.js
@@ -10,7 +10,7 @@ Exported Functions: AdminEvents
 Contributors:
 	Philip Pavlick - 02/18/23 - SP-312
   Philip Pavlick - 02/25/23 - SP-314
-
+  Philip Pavlick - 02/26/23 - SP-388
 ===================================================================+*/
 
 import React, { useState } from 'react';
@@ -20,6 +20,7 @@ import { useEffect } from 'react';
 import { fnGetEventRequests} from '../DBFunctions';
 import { onAuthStateChanged } from 'firebase/auth';
 import { oAuthentication } from '../firebase-config';
+import moment from 'moment';
 
 
 
@@ -47,27 +48,33 @@ export default function AdminEvents() {
 
     return(
       <Box >
-      {iEvents.map(iEvents => ( 
-        <Box sx={{ m: 9, border: 1 }} >
-          <Grid container spacing={2}>
-            <Grid item xs={6}>
-              <div>Left</div>
+        {iEvents.map(iEvent => ( 
+          <Box sx={{ m: 9, border: 1, borderRadius: '4px'}} key={iEvent[0].event_id}>
+            <Grid container spacing={2}>
+              <Grid item xs={4}>
+                <div>{iEvent[0].event_name}</div>
+              </Grid>
+              <Grid item xs={7}>
+                <div> Sample Location</div>
+              </Grid>
+              <Grid item xs={1}>
+                <div></div>
+              </Grid>
+              <Grid item xs={4}>
+                <div>Sample Organization</div>
+              </Grid>
+              <Grid item xs={7}>
+                <div> { moment( iEvent[0].event_timestamp.seconds * 1000 + iEvent[0].event_timestamp.nanoseconds / 1000000 ).format("dddd, MMMM Do YYYY, h:mm a")  }</div>
+              </Grid>
+              <Grid item xs={1}>
+                <div></div>
+              </Grid>
             </Grid>
-            <Grid item xs={5}>
-              <div>Middle</div>
-            </Grid>
-            <Grid item xs={1}>
-              <div>Right</div>
-            </Grid>
-          </Grid>
-        </Box>
+          </Box>
 
-      ))}
+        ))}
+
       </Box>
 
     );
   } 
-
-
-
-
