1f181d348480d13f5c00f8f726b1f6ca8c920340
Sam M <samuel.merlin@stvincent.edu>
2023-02-28 19:38:28 -0500

    Update OrgE.js

--- web-dashboard/src/Pages/OrgE.js
+++ web-dashboard/src/Pages/OrgE.js
@@ -1,5 +1,5 @@
 /*+===================================================================
-File: OrgAnn.js
+File: OrgE.js
 
 Summary: A holder page for the organization Events page
 
@@ -14,20 +14,26 @@ Contributors:
 import React, {useState} from 'react';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 import { useEffect } from 'react';  
-import { fnGetEventRequests} from '../DBFunctions';
+import { fnGetOrganizationEvents} from '../DBFunctions';
 import { onAuthStateChanged } from 'firebase/auth';
 import { oAuthentication } from '../firebase-config';
+import moment from 'moment';
 import './OrgList.css'      
 import {useLocation} from 'react-router-dom'
 
 export default function Events() {
 
-    const [iEvents, setiEvents] = useState([]);
+  const location = useLocation()
+  const data = location.state.data;
 
+    const [iEvents, setiEvents] = useState([]);
+  console.log(data.id)
     useEffect(() => {
       const fnRenderEvents = async () => {
-        let oEvents = await fnGetEventRequests();
+        let oEvents = await fnGetOrganizationEvents(data.id);
+        console.log(oEvents)
         setiEvents(oEvents);
       }
       
@@ -38,19 +44,20 @@ export default function Events() {
       });
     }, []);
 
-
-    const location = useLocation()
-    const data = location.state.data;
     return(
         <Box sx={{ m: 9 }} >
-            <Grid container spacing={{ xs: 9, md: 5 }} columnSpacing = {4}>
+            <Grid container spacing={{ xs: 9, md: 2 }} columnSpacing = {4}>
                 {iEvents.map(iEvent => ( 
-                    <Grid textAlign='center' key={iEvent[0].event_id}>
-                        <Box sx={{p: 6, m:3, border: 1, borderRadius: '4px'}}>
-                            <div>HII</div>
-                            <div>{iEvent[0].event_name}</div>
-                        </Box>
-                        <div>{iEvent[0].event_name}</div>
+                    <Grid textAlign='center' key={iEvent.event_id}>
+                        <Box sx={{p: 2, m:2, border: 1, borderRadius: '8px'}}>   
+                        <div>{iEvent.event_location}</div>         
+                            <div> { moment( iEvent.event_timestamp.seconds * 1000 + iEvent.event_timestamp.nanoseconds / 1000000 ).format("MMM Do YY, h:mm a")  }</div>
+                                <div>
+                                  <Button sx={{ m: 1, color: 'black', backgroundColor: 'green', border: 1 }} >Edit</Button ><Button sx={{  color: 'black', backgroundColor: 'red', border: 1 }}>Delete</Button>
+                                </div>
+                                <div>{iEvent.event_status}</div>
+                            </Box>
+                        <div>{iEvent.event_name}</div>
                     </Grid>
                 ))}
             </Grid>
