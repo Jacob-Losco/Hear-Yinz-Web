83168d56322941b22fede9926f92bac62ded0017
Sam M <samuel.merlin@stvincent.edu>
2023-03-07 16:13:16 -0500

    FunctionalityBegin

--- web-dashboard/src/Pages/EventForm.js
+++ web-dashboard/src/Pages/EventForm.js
@@ -17,13 +17,38 @@ import '@fontsource/dm-sans';
 import '../font.css';
 import './EventForm.css';
 import {useLocation} from 'react-router-dom'
+import moment from 'moment';
 
-function GetName(name){
+function GetEventName(name){
     if (name){
         return(name.event_name);
     }
     else{
-        return(<div></div>)
+        return
+    }
+}
+function GetEventTime(time){
+    if (time){
+        return( moment( time.event_timestamp.seconds * 1000 + time.event_timestamp.nanoseconds / 1000000 ).format("MMM Do YY, h:mm a")  );
+    }
+    else{
+        return
+    }
+}
+function GetEventlocation(locations){
+    if (locations){
+        return(locations.location.location_name);
+    }
+    else{
+        return
+    }
+}
+function GetEventDescription(descript){
+    if (descript){
+        return(descript.event_description);
+    }
+    else{
+        return
     }
 }
 
@@ -38,43 +63,48 @@ export default function AddEventForm() {
     const [sEventDescription, fndSetEventDescription] = useState("");
     const [sEventStatus, fndSetEventStatus] = useState("");
 
+    const handleChange = (event) => {
+        // Get input value from "event"
+        //setMessage(event.target.value);
+      };
+
     return(
-        <div className="OrganizationEventFormContainer">
-           <div className="EventFormContainer">
-                 <div className="EventFormUsDataContainer">
-                    <div className="EventFormInputContainer">
-                        <div>
-                            <label className='text'>Name</label> 
-                                <input value={sEventName} className = "EventNameInput"></input> 
-                          </div>
-                        <div>
-                            <label for='eventDateTime' className="text">Date and Time</label>
-                                <input className = "EventTimeInput" type="datetime-local" id="eventDateTime" name="eventDateTime"></input>
-                            </div>
-                        <div>
-                            <label  for="loc" className="text">Location</label>
-                                <select name="loc" id="onSite" className = 'EventLocationSelectInput' >
-                                    <option value="" disabled selected>Select your option</option>
-                                    <option value="loc1">loc1</option>
-                                    <option value="loc2">loc2</option>
-                                    <option value="loc3">loc3</option>
-                                </select>
-                          </div>
-                        <div>
-                            <label className="text">Description</label>
-                                <textarea value={sEventDescription} className="EventDescriptionInput"></textarea>
-                          </div>
-                          <div>
-                            <input type="radio" name="action" className='RadioBtn'/> <label className='RadioLabel'>Public</label>
-                            <input type="radio" name="action" className='RadioBtn'/> <label className='RadioLabel'>Private &#40;Followers Only&#41;</label>
-                        </div>
-                        <div>
-                            <button className='cancelBtn'>Cancel</button>
-                            <button className='submitBtn'>Create</button>
-                        </div>
-                    </div>
+<div className="OrganizationEventFormContainer">
+    <div className="EventFormContainer">
+        <div className="EventFormUsDataContainer">
+        <div className="EventFormInputContainer">
+            <div>
+                <label className='text'>Name</label> 
+                    <input value={GetEventName(EventInfo)} className = "EventNameInput" onChange={handleChange}></input> 
                 </div>
+            <div>
+                <label for='eventDateTime' className="text">Date and Time</label>
+                    <input value={GetEventTime(EventInfo)} className = "EventTimeInput" type="datetime-local" id="eventDateTime" name="eventDateTime" onChange={handleChange} ></input>
+                </div>
+            <div>
+                <label  for="loc" className="text">Location</label>
+                    <select value={GetEventlocation(EventInfo)} name="loc" id="onSite" className = 'EventLocationSelectInput' onChange={handleChange}>
+                        <option value="" disabled selected>Select your option</option>
+                        <option value="loc1">loc1</option>
+                        <option value="loc2">loc2</option>
+                        <option value="loc3">loc3</option>
+                    </select>
+                </div>
+            <div>
+                <label className="text">Description</label>
+                    <textarea value={GetEventDescription(EventInfo)} className="EventDescriptionInput" onChange={handleChange}></textarea>
+                </div>
+                <div>
+                <input type="radio" name="action" className='RadioBtn'/> <label className='RadioLabel'>Public</label>
+                <input type="radio" name="action" className='RadioBtn'/> <label className='RadioLabel'>Private &#40;Followers Only&#41;</label>
+            </div>
+            <div>
+                <button className='cancelBtn'>Cancel</button>
+                <button className='submitBtn'>Create</button>
             </div>
         </div>
+    </div>
+</div>
+</div>
     );
 }
