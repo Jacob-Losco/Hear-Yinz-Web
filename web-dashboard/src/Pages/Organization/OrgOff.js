b9a26cfb45c062eda6788763a49bfb5f4f30ccab
Sam M <samuel.merlin@stvincent.edu>
2023-03-17 18:23:13 -0400

    Functionality

--- web-dashboard/src/Pages/Organization/OrgOff.js
+++ web-dashboard/src/Pages/Organization/OrgOff.js
@@ -11,23 +11,52 @@ Contributors:
     Sam Merlin 2/21/2023 - SP 269
 
 ===================================================================+*/
-import React from 'react';
+import React,{ useState} from 'react';
 import {useLocation} from 'react-router-dom'
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
-import '../../Styles/OrgOff.css'   
+import '../../Styles/OrgOff.css';
+import {fnAddOfficer} from   '../../DBFunctions'
 export default function Officers() {
+
+    const [sOfficerEmail, fnSetOfficerEmail] = useState("");
+
     const location = useLocation()
-    const data = location.state.data;
+    const OrgInfo = location.state.data;
+
+    const fnHandleAddOfficer = async () => {
+        const oMessage = document.querySelector(".AnnouncementMessage");
+        if(sOfficerEmail == "") {
+            oMessage.innerHTML = "Invalid input. Please enter an email!"
+        } else {
+            const error = await fnAddOfficer(OrgInfo.id, {
+                account_email: sOfficerEmail
+            });
+            if(error) {
+                console.log(error);
+                oMessage.innerHTML = "Invalid Email. Ensure the user exists in your institution or contact administration!";
+            } else {
+                document.querySelector(".emInput").value = "";
+                oMessage.innerHTML = "Successfully Added Officer!";
+            }
+        }
+    }
     return(
         <div className='offFormContainter'>
             <div className='offFormDataContainer'>
                 <Box component="div" sx={{ display: 'inline' }}>
                     <label className='emailInput'>Officer Email</label> 
-                        <input className='emInput'></input>
-                     <Button sx={{  color: 'white', backgroundColor: '#38741D', border: 1, borderRadius: 15 }}> + </Button> 
+                        <input className='emInput'onChange={(event) => {
+                        fnSetOfficerEmail(event.target.value);}}></input>
+                     <Button sx={{  color: 'white', backgroundColor: '#38741D', border: 1, borderRadius: 15,  borderColor:'black' }}
+                     onClick={fnHandleAddOfficer}> + </Button> 
                 </Box>
+                <div className='InnerHTML'>
+                <p className="AnnouncementMessage"></p>
             </div>
+            </div>
+
+
         </div>
     );
 }
