68ae80a592e0225140fe5c9136d66b9d912de39e
Sam M <samuel.merlin@stvincent.edu>
2023-03-18 12:12:22 -0400

    OrgOfficerPageTest

---
+++ web-dashboard/src/Tests/OrgOfficerPage.test.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import {render, screen, fireEvent, waitFor, cleanup} from '@testing-library/react';
+import { BrowserRouter} from 'react-router-dom';
+import Login from "../Login";
+import NavBar from "../NavBar";
+import 'mutationobserver-shim';
+import { fnLogout } from '../LoginFunctions';
+
+afterEach(cleanup);
+describe("OrgOfficerPageTesting", () => {
+
+    /*T+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+      Test: Organizations page Elements For the Officer
+
+      Target: Organizations
+
+      Assertions: On screen element of the event name loads, orgevent is shown 
+
+      Writer: Sam Merlin 2/25/2023 SP-270 SP-275
+    -------------------------------------------------------------------T*/
+
+test("Organizations page officer ", async () => {
+  jest.setTimeout(30000)
+  render(
+  <NavBar />, 
+  { wrapper: BrowserRouter},
+  <Login
+  button
+  fnSetLoginEmail={()=>{}}
+  LoginPassword={[]}
+  fnSetLoginPassword={()=>{}}/>
+  );
+   
+  const oSubmitButton = screen.getByRole('button',{name: /log in/i}); 
+  const oInputElementEmail = screen.getByPlaceholderText(/email/i);
+  const oInputElementPassword = screen.getByPlaceholderText(/password/i);
+
+  fireEvent.change(oInputElementEmail, { target: { value: "teststatic_officer@teststatic.edu"} });
+  fireEvent.change(oInputElementPassword, { target: { value: "test123"} });
+  fireEvent.click(oSubmitButton);
+
+
+  const OrgName =  await waitFor( () => screen.getByText('TestStatic_Org1'), { timeout: 8000 });
+  const oOrgButton = screen.getByAltText("orgImg");
+  fireEvent.click(oOrgButton); 
+  const OrgName1 =  await waitFor( () => screen.getByText('TestStatic_Org1'), { timeout: 8000 });
+  const oRequestsEventButton = screen.getByRole('link', {name: /Officer/i}); 
+  fireEvent.click(oRequestsEventButton);
+
+  const EventRequestItem = await waitFor( () => screen.findByText("Officer Email"), { timeout: 8000 });
+  expect (EventRequestItem).toBeInTheDocument();
+
+  await fnLogout();
+});
+});
